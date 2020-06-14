bb6341a62f4bd892ec927d05b91eff656e0c683f
Cale <caleworkman@gmail.com>
2020-06-14 00:11:09 -0400

    Updates

---
+++ precache-manifest.604bd2ffe3b5ebe72ef184386d792130.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e758386e58f26c7e25904ffb9740722",
+    "url": "/ifit-interview-challenge/index.html"
+  },
+  {
+    "revision": "d5fdfd58e010ddaabc2e",
+    "url": "/ifit-interview-challenge/static/css/main.ef5f88e6.chunk.css"
+  },
+  {
+    "revision": "ceea3b8a58ab215c1776",
+    "url": "/ifit-interview-challenge/static/js/2.64e1d896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ifit-interview-challenge/static/js/2.64e1d896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fdfd58e010ddaabc2e",
+    "url": "/ifit-interview-challenge/static/js/main.de93bdb3.chunk.js"
+  },
+  {
+    "revision": "6180c93b17bc92752f03",
+    "url": "/ifit-interview-challenge/static/js/runtime-main.9879db51.js"
+  },
+  {
+    "revision": "281898012ada430acf9e7410d5c3499c",
+    "url": "/ifit-interview-challenge/static/media/arrow.28189801.svg"
+  },
+  {
+    "revision": "69d5a2bdc62a05f6d6bd63e8521ec1bb",
+    "url": "/ifit-interview-challenge/static/media/equipment-bikes.69d5a2bd.png"
+  },
+  {
+    "revision": "de0856a4c96756fd802e4a755a9ca586",
+    "url": "/ifit-interview-challenge/static/media/equipment-ellipticals.de0856a4.png"
+  },
+  {
+    "revision": "0a5e3a7ad0ca806b6bdb4886879039f4",
+    "url": "/ifit-interview-challenge/static/media/equipment-strength.0a5e3a7a.png"
+  },
+  {
+    "revision": "afbef8a6d98062146b7a91380fa4259a",
+    "url": "/ifit-interview-challenge/static/media/equipment-treadmills.afbef8a6.png"
+  },
+  {
+    "revision": "11c162a089ba287d83f146e5b79e8680",
+    "url": "/ifit-interview-challenge/static/media/expand-less.11c162a0.svg"
+  },
+  {
+    "revision": "ca15223b7e9d145e0d43f5c655689e3b",
+    "url": "/ifit-interview-challenge/static/media/expand-more.ca15223b.svg"
+  },
+  {
+    "revision": "7af5d0f6aafca3b2d2f7277e35ca0a1b",
+    "url": "/ifit-interview-challenge/static/media/gear-junkie-logo.7af5d0f6.svg"
+  },
+  {
+    "revision": "2f431304e8ceb18cecaa45e7b511680f",
+    "url": "/ifit-interview-challenge/static/media/gmapsinworkout.2f431304.png"
+  },
+  {
+    "revision": "8db55f1f529af436129fe233ac53cf07",
+    "url": "/ifit-interview-challenge/static/media/ifit-coach-logo.8db55f1f.svg"
+  },
+  {
+    "revision": "1ac4484b698f8ea2239e5f86cbde622f",
+    "url": "/ifit-interview-challenge/static/media/market-1.1ac4484b.png"
+  },
+  {
+    "revision": "c7f5d823176926808bd7e4a58e4c1dcc",
+    "url": "/ifit-interview-challenge/static/media/market-2.c7f5d823.png"
+  },
+  {
+    "revision": "2adb7ee8d66c2b9bbabfe94ebadcb0ab",
+    "url": "/ifit-interview-challenge/static/media/market-3.2adb7ee8.png"
+  },
+  {
+    "revision": "4e5afafc873910c05c51b58f630e74c6",
+    "url": "/ifit-interview-challenge/static/media/market-4.4e5afafc.png"
+  },
+  {
+    "revision": "8270da1a174e9e29ec741f253d5259d4",
+    "url": "/ifit-interview-challenge/static/media/market-5.8270da1a.png"
+  },
+  {
+    "revision": "69b52d2bc0c31e751b19bf5702410a47",
+    "url": "/ifit-interview-challenge/static/media/market-6.69b52d2b.png"
+  },
+  {
+    "revision": "f8a1d38ed1a57150a8756ecdd188e265",
+    "url": "/ifit-interview-challenge/static/media/market-7.f8a1d38e.png"
+  },
+  {
+    "revision": "4818a5bee5229836f7f8adb83ffc7142",
+    "url": "/ifit-interview-challenge/static/media/market-8.4818a5be.png"
+  },
+  {
+    "revision": "26d0498004c4b4260470689683392ae3",
+    "url": "/ifit-interview-challenge/static/media/mashable-logo.26d04980.svg"
+  },
+  {
+    "revision": "2ad37b94badf3822c43fa5d68199641a",
+    "url": "/ifit-interview-challenge/static/media/wired-logo.2ad37b94.svg"
+  }
+]);
