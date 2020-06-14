5a00993bb0c53a9116d4e98caa7ab20149ded701
Cale <caleworkman@gmail.com>
2020-06-14 15:04:54 -0400

    Remove animation from the ifit-enabled equipment title.

--- src/components/equipment/Equipment.js
+++ src/components/equipment/Equipment.js
@@ -16,13 +16,13 @@ class Equipment extends PureComponent {
     });
 
     return (
-      <div
-        className={"equipment" + (this.props.isVisible ? " equipment--visible" : "")}
-        ref={this.props.equipmentRef}>
+      <div className="equipment">
         <div className="equipment__title">
           Interested in our exciting iFit-enabled equipment?
         </div>
-        <div className="equipment__grid">
+        <div
+          className={"equipment__grid" + (this.props.isVisible ? " equipment__grid--visible" : "")}
+          ref={this.props.equipmentRef}>
           {equipment}
         </div>
       </div>
